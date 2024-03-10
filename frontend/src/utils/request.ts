7aed22059be0e0f4d7cac09a49d59f8af0e18f32
Mohamed-gp <mohamedterba6@gmail.com>
2024-03-10 18:24:22 +0100

    add include

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://auth-5.onrender.com/",
   credentials: "include",
+  withCredentials: true,
 });
 
 export { request };
